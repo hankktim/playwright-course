044b23a62fc2b2d4723619555c91c0f5457dbc27
hankktim <70684996+hankktim@users.noreply.github.com>
2026-09-17 09:38:18 +0300

    set headless: true,

--- Starter/playwright.config.ts
+++ Starter/playwright.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   },
   use: {
     baseURL: 'http://localhost:5000/',
-    headless: false,
+    headless: true,
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
     trace: 'retain-on-failure',
